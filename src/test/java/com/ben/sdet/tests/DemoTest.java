b6c792a8ff1c2436606f662c9b4b5afb79ca1aec
Benjamin Piscopo <benjap@Benjamins-MacBook-Pro.local>
2026-04-12 23:28:06 -0300

    initial project setup with dependencies and configurations

---
+++ src/test/java/com/ben/sdet/tests/DemoTest.java
@@ -0,0 +1,13 @@
+package com.ben.sdet.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+
+    @Test
+    public void demoTest() {
+        Assert.assertTrue(true, "This test should always pass");
+    }
+
+}
