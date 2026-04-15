a0cfe798f1ee6dba10184be9642144c7cb08fb2b
Benjamin Piscopo <benjap@Benjamins-MacBook-Pro.local>
2026-04-15 14:48:31 -0300

    feat: add TestNG report exclusion to .gitignore and update CreateUserRequest with Lombok annotations

--- src/main/java/com/ben/sdet/dto/user/CreateUserRequest.java
+++ src/main/java/com/ben/sdet/dto/user/CreateUserRequest.java
@@ -1,10 +1,14 @@
 package com.ben.sdet.dto.user;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserRequest {
 
     private String name;
